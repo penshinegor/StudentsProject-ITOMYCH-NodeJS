ab077914352b9a9341bafada3593e5a82e5ef7d5
Yehor Penshyn <pensinegor@gmail.com>
2022-12-06 20:32:38 +0200

    Creating enum to store available options

---
+++ components/enums/heroes-enums.ts
@@ -0,0 +1,4 @@
+enum KindOfAdvantage { Strength = 'Strength', Agility = 'Agility', Intelligence = 'Intelligence' }
+enum TypeOfHero { DragonWarrior = 'Dragon Warrior', Thief = 'Thief', Wizard = 'Wizard' }
+
+export {KindOfAdvantage, TypeOfHero}
