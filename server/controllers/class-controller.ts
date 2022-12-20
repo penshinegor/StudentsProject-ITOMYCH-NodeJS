60e7ad0aa318a2ea067f84130fd3f9138116d509
Yehor Penshyn <pensinegor@gmail.com>
2022-12-20 20:51:26 +0200

    Moving User and Class Controller

--- server/controllers/class-controller.ts
+++ server/controllers/class-controller.ts
@@ -1,42 +1,14 @@
-function dispatchEvent(ws, event) {
-    const json = JSON.parse(event);
-    switch (json.event) {
-        case 'attack': {
-            if (!json.id) {
-                ws.send('Uncorrected data');
-            } else {
-                ws.send(`Applying attack ${json.id} user`);
-            }
-            break;
-        }
-        case 'apply-ability': {
-            if (!json.id) {
-                ws.send('Uncorrected data');
-            } else {
-                ws.send(`Applying ability ${json.id} user`);
-            }
-            break;
-        }
-        case 'reincarnation': {
-            if (!json.id) {
-                ws.send('Uncorrected data');
-            } else {
-                ws.send(`Reincarnation ${json.id} user`);
-            }
-            break;
-        }
-        case 'message': {
-            if (!json.text) {
-                ws.send('Uncorrected data');
-            } else {
-                ws.send(`Sending message: ${json.text}`);
-            }
-            break;
-        }
-        default: {
-            ws.send('Wrong query');
-        }
+import ClassService from '../services/class-service';
+
+const classService = new ClassService();
+
+const getListOfHeroesController = (req, res) => {
+    const list = classService.getListOfHeroes();
+    if (!list) {
+        res.status(500).send('Getting error');
+        return;
     }
+    res.json(list);
 }
 
-export {dispatchEvent};
+export {getListOfHeroesController};
